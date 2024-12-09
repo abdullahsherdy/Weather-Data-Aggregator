707baf3503bde76c06d037e6419d6932144a1ebe
Abdullah Ahmed <abdallahsherdy@gmail.com>
2024-12-09 17:53:05 +0200

    Setup Project Environment

---
+++ WeatherDataAggregator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WeatherDataAggregator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
